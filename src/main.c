6e62a7185de5003758a2c5fb4ba84c55ab750596
Awnis Dichou <adichou@c1r3p9.42perpignan.fr>
2025-10-18 19:26:45 +0200

    2

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: adichou <adichou@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/07 20:58:41 by adichou           #+#    #+#             */
-/*   Updated: 2025/10/15 19:03:51 by adichou          ###   ########.fr       */
+/*   Updated: 2025/10/18 19:13:00 by adichou          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,9 +19,16 @@ typedef struct s_point
 	float	z;
 }	t_point;
 
+typedef	struct s_plan
+{
+	t_point	a;
+	t_point	b;
+	t_point	c;
+}	t_plan;
+
 typedef struct s_vector
 {
-	t_point		vector;
+	t_point		vector_point;
 	t_point		from;
 }	t_vector;
 
@@ -37,10 +44,16 @@ typedef struct s_cylinder
 	float		radius;
 }	t_cylinder;
 
-t_point	calcul_norm(t_vector vec_2, vec_2)
+t_vector	create_vector(t_point from, t_point to)
 {
-	t_point		res;
-	res
+	t_vector	res;
+	t_point		tmp;
+
+	tmp.x = to.x - from.x;
+	tmp.y = to.y - from.y;
+	tmp.z = to.z - from.z;
+	res.vector_point = tmp;
+	res.from = from;
 }
 
 float	calcul_prod_scal(t_vector vec_1, t_vector vec_2)
@@ -53,6 +66,19 @@ float	calcul_prod_scal(t_vector vec_1, t_vector vec_2)
 	y_temp = vec_1.from.y * vec_2.from.y;
 	z_temp = vec_1.from.z * vec_2.from.z;
 	return (x_temp + y_temp + z_temp);
+}
+
+t_point	calcul_plan_norm(t_plan plan)
+{
+	t_vector		res;
+	t_vector		ab;
+	t_vector		ac;
+
+	ab = create_vector(plan.a, plan.b);
+	ac = create_vector(plan.a, plan.c);
+	// scal(res, ab) doit etre egal a 0;
+	// scal(res, ac) doit etre egal a 0;
+	res =  
 }
 
 int	main(int argc, char **argv)
