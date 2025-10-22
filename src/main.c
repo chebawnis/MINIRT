39b56331b4384236ea2bc7d44ba9dc88cd0e713c
Awnis Dichou <adichou@c3r5p8.42perpignan.fr>
2025-10-22 20:24:14 +0200

    commit3

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: adichou <adichou@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/07 20:58:41 by adichou           #+#    #+#             */
-/*   Updated: 2025/10/18 19:13:00 by adichou          ###   ########.fr       */
+/*   Updated: 2025/10/22 20:23:43 by adichou          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -54,6 +54,7 @@ t_vector	create_vector(t_point from, t_point to)
 	tmp.z = to.z - from.z;
 	res.vector_point = tmp;
 	res.from = from;
+	return (res);
 }
 
 float	calcul_prod_scal(t_vector vec_1, t_vector vec_2)
@@ -62,9 +63,9 @@ float	calcul_prod_scal(t_vector vec_1, t_vector vec_2)
 	float	y_temp;
 	float	z_temp;
 
-	x_temp = vec_1.from.x * vec_2.from.x;
-	y_temp = vec_1.from.y * vec_2.from.y;
-	z_temp = vec_1.from.z * vec_2.from.z;
+	x_temp = vec_1.vector_point.x * vec_2.vector_point.x;
+	y_temp = vec_1.vector_point.y * vec_2.vector_point.y;
+	z_temp = vec_1.vector_point.z * vec_2.vector_point.z;
 	return (x_temp + y_temp + z_temp);
 }
 
@@ -78,7 +79,64 @@ t_point	calcul_plan_norm(t_plan plan)
 	ac = create_vector(plan.a, plan.c);
 	// scal(res, ab) doit etre egal a 0;
 	// scal(res, ac) doit etre egal a 0;
-	res =  
+	res.vector_point.x = ab.vector_point.y * ac.vector_point.z - ab.vector_point.z * ac.vector_point.y;
+	res.vector_point.y = ab.vector_point.z * ac.vector_point.x - ab.vector_point.x * ac.vector_point.z;
+	res.vector_point.z = ab.vector_point.x * ac.vector_point.y - ab.vector_point.y * ac.vector_point.x;
+	res.from.x = 0;
+	res.from.y = 0;
+	res.from.z = 0;
+	return (res.vector_point);
+}
+
+t_vector	sub_vec(t_vector v1, t_vector v2)
+{
+	t_vector	res;
+
+	res.vector_point.x = v1.vector_point.x - v2.vector_point.x;
+	res.vector_point.y = v1.vector_point.y - v2.vector_point.y;
+	res.vector_point.z = v1.vector_point.z - v2.vector_point.z;
+	return (res);
+}
+
+t_vector	mult_vec(float n, t_vector v1)
+{
+	t_vector	res;
+
+	res.vector_point.x = v1.vector_point.x * n;
+	res.vector_point.y = v1.vector_point.y * n;
+	res.vector_point.z = v1.vector_point.z * n;
+	return (res);
+}
+
+t_vector	bounce_plan(t_vector v, t_vector norm_plan)
+{
+	t_vector	res;
+
+	// res = v - 2(prod_scal(v, norm))norm
+	res = sub_vec(v, mult_vec(2 * calcul_prod_scal(v, norm_plan), norm_plan));
+	return (res);
+}
+
+int	launch_vec(vector, indice)
+{
+	// vecteur doit partir de camera_fov et passer par i,
+	//
+	// verifier le premier objet touche par le vecteur, noter le point de collision
+	// et noter la nature de l'objet,
+	//
+	// launch_vec(bounce(vector, point_collision, objet*), indice - reflexion_index)
+	// * "plan", "circle" ou "cylindre" (pour l'instant) 
+}
+
+int	lauch_map(map)
+{
+	while (i < total_pixel)
+	{
+		lauch_vec(get_vector(i, camera_fov));
+		i ++;
+		//passer au pixel d'apres
+	}
+	
 }
 
 int	main(int argc, char **argv)
