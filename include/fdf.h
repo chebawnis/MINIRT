6b1f1ba330fea120d02f8d5152059228a7cbad1f
Awnis Dichou <adichou@c2r1p1.42perpignan.fr>
2025-10-24 00:23:07 +0200

    oaijsd

--- include/fdf.h
+++ include/fdf.h
@@ -6,7 +6,7 @@
 /*   By: adichou <adichou@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/07 21:27:34 by adichou           #+#    #+#             */
-/*   Updated: 2025/09/18 23:29:36 by adichou          ###   ########.fr       */
+/*   Updated: 2025/10/23 23:03:24 by adichou          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,3 +16,67 @@
 #include <math.h>
 #include <fcntl.h>
 #include <stdlib.h>
+
+# ifndef RES_X
+#  define RES_X 1920
+# endif
+# ifndef RES_Y
+#  define RES_Y 1080
+# endif
+
+typedef struct s_point
+{
+	float		x;
+	float		y;
+	float		z;
+}	t_point;
+
+typedef	struct s_plan
+{
+	t_point		a;
+	t_point		b;
+	t_point		c;
+}	t_plan;
+
+typedef struct s_vector
+{
+	t_point		vector_point;
+	t_point		from;
+}	t_vector;
+
+typedef struct s_sphere
+{
+	float		center[3];
+	float		radius;
+}	t_sphere;
+
+typedef struct s_cylinder
+{
+	t_vector	center;
+	float		radius;
+}	t_cylinder;
+
+typedef struct s_camera
+{
+	t_vector	camera;
+	float		fov;
+}	t_camera;
+
+typedef struct s_mlx
+{
+	void		*mlx;
+	void		*win;
+	void		*img;
+	char		*addr;
+	int			bpp;
+	int			line_length;
+	int			endian;
+}	t_mlx;
+
+typedef struct s_program
+{
+	int			x_size;
+	int			y_size;
+	t_camera	camera;
+	t_mlx		mlx;
+}	t_program;
--- src/main.c
+++ src/main.c
@@ -6,43 +6,12 @@
 /*   By: adichou <adichou@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/07 20:58:41 by adichou           #+#    #+#             */
-/*   Updated: 2025/10/22 20:23:43 by adichou          ###   ########.fr       */
+/*   Updated: 2025/10/24 00:22:02 by adichou          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/fdf.h"
 
-typedef struct s_point
-{
-	float	x;
-	float	y;
-	float	z;
-}	t_point;
-
-typedef	struct s_plan
-{
-	t_point	a;
-	t_point	b;
-	t_point	c;
-}	t_plan;
-
-typedef struct s_vector
-{
-	t_point		vector_point;
-	t_point		from;
-}	t_vector;
-
-typedef struct s_sphere
-{
-	float		center[3];
-	float		radius;
-}	t_sphere;
-
-typedef struct s_cylinder
-{
-	t_vector	center;
-	float		radius;
-}	t_cylinder;
 
 t_vector	create_vector(t_point from, t_point to)
 {
@@ -117,33 +86,101 @@ t_vector	bounce_plan(t_vector v, t_vector norm_plan)
 	return (res);
 }
 
-int	launch_vec(vector, indice)
+t_vector	get_vector(t_point a, t_point b)
 {
-	// vecteur doit partir de camera_fov et passer par i,
-	//
-	// verifier le premier objet touche par le vecteur, noter le point de collision
-	// et noter la nature de l'objet,
-	//
-	// launch_vec(bounce(vector, point_collision, objet*), indice - reflexion_index)
-	// * "plan", "circle" ou "cylindre" (pour l'instant) 
+	t_vector	res;
+
+	res.vector_point.x = b.x - a.x;
+	res.vector_point.y = b.y - a.y;
+	res.vector_point.z = b.z - a.z;
+	return (res);
 }
 
-int	lauch_map(map)
-{
-	while (i < total_pixel)
-	{
-		lauch_vec(get_vector(i, camera_fov));
-		i ++;
-		//passer au pixel d'apres
-	}
+// int	launch_vec(vector, indice)
+// {
 	
+// 	// vecteur doit partir de camera_fov et passer par i,
+// 	//
+// 	// verifier le premier objet touche par le vecteur, noter le point de collision
+// 	// et noter la nature de l'objet,
+// 	//
+// 	// launch_vec(bounce(vector, point_collision, objet*), indice - reflexion_index)
+// 	// * "plan", "circle" ou "cylindre" (pour l'instant) 
+// }
+
+// int	lauch_map(map)
+// {
+	
+// 	while (i < total_pixel)
+// 	{
+// 		lauch_vec(get_vector(i, camera_fov));
+// 		i ++;
+// 		//passer au pixel d'apres
+// 	}
+	
+// }
+
+t_point	create_point(float x, float y, float z)
+{
+	t_point	res;
+
+	res.x = x;
+	res.y = y;
+	res.z = z;
+	return (res);
+}
+
+void	init_program(t_program *program)
+{
+	program->x_size = RES_X;
+	program->y_size = RES_Y;
+	program->camera.camera = create_vector(create_point(10, 0, 10),
+							create_point(10, program->camera.fov, 10));
+	program->mlx.mlx = mlx_init();
+	program->mlx.win =  mlx_new_window(program->mlx.mlx, RES_X, RES_Y, "MiniRT");
+	program->mlx.img = mlx_new_image(program->mlx.mlx, RES_X, RES_Y);
+	program->mlx.addr = mlx_get_data_addr(program->mlx.img, &(program->mlx.bpp),
+						&(program->mlx.line_length), &(program->mlx.endian));
+}
+
+void	put_pixel(char *addr, int line_length, int bpp, int x, int y, int color)
+{
+	char	*pixel;
+
+	pixel = addr + (y * line_length + x * (bpp / 8));
+	*(unsigned int *)pixel = color;
+}
+
+void	pixelput(t_mlx mlx, int x, int y)
+{
+	put_pixel(mlx.addr, mlx.line_length, mlx.bpp, x, y, 0xFF0000);
+}
+
+int	minirt(void)
+{
+	t_program	*program;
+
+	program = malloc(sizeof(t_program));
+	init_program(program);
+
+	// PROGRAMME PRINCIPAL
+	
+	
+	pixelput(program->mlx, 400, 400);
+	mlx_put_image_to_window(program->mlx.mlx, program->mlx.win,
+							program->mlx.img, 0, 0);
+
+
+
+	mlx_loop(program->mlx.mlx);
+	return (0);
 }
 
 int	main(int argc, char **argv)
 {
-	if (argc == 2)
+	if (argc == 1)
 	{
-		printf("argc est egal a 2");
+		minirt();
 		(void)argv[1];
 	}
 	return (0);
